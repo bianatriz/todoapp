d5a48917f642a901ded10d3970d3efda6babd424
unisanta <unisanta@santacecilia.br>
2025-03-12 18:08:08 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ComponentRef } from '../../node_modules/@angular/core/index';
+import { Tarefa} from "./tarefa";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  constructor() {
+  this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: HTMLInputElement) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa.value, false);
+    this.arrayDeTarefas.unshift(novaTarefa);
+
+    descricaoNovaTarefa.value = "";
+    }   
+
+  READ_tarefas() {
+  this.arrayDeTarefas = [
+  new Tarefa("Estudar Frameworks WEB", false),
+  new Tarefa("Comer Pizza", false),
+  new Tarefa("Ajudar meus pais", false)
+  ];
+  }
+
+  DELETE_tarefa(tarefaAserRemovida : Tarefa){
+    this.arrayDeTarefas.splice(this.arrayDeTarefas.indexOf(tarefaAserRemovida),1);
+  }
+ 
 }
